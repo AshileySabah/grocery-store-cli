fb9d91418ec2d3aa3cac18904fb121e7e8fa3cd4
AshileySabah <ashileysabrina2002@gmail.com>
2022-06-10 19:49:11 -0300

    feat: home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,37 +1,8 @@
 import React from 'react';
-import {StyleSheet, Text, View, Image} from 'react-native';
-
-import logo from '../../assets/logo.png';
+import Produtores from './Produtores';
 
 const Home = () => {
-  return (
-    <View style={styles.topo}>
-      <Image style={styles.imagem} source={logo} />
-      <Text style={styles.boasVindas}>Olá, Maria</Text>
-      <Text>Encontre os melhores produtores</Text>
-    </View>
-  );
+  return <Produtores />;
 };
 
 export default Home;
-
-const styles = StyleSheet.create({
-  topo: {
-    backgroundColor: '#f6f6f6',
-    padding: 16,
-  },
-  imagem: {
-    width: 70,
-    height: 28,
-  },
-  boasVindas: {
-    marginTop: 24,
-    fontSize: 26,
-    lineHeight: 42,
-    fontWeight: 'bold',
-  },
-  legenda: {
-    fontSize: 16,
-    lineHeight: 26,
-  },
-});
