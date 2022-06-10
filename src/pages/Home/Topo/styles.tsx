9494c68f28d75246b1170a525e3ff42c23df877f
AshileySabah <ashileysabrina2002@gmail.com>
2022-06-10 19:44:37 -0300

    feat: topo

---
+++ src/pages/Home/Topo/styles.tsx
@@ -0,0 +1,24 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  topo: {
+    backgroundColor: '#f6f6f6',
+    padding: 16,
+  },
+  imagem: {
+    width: 70,
+    height: 28,
+  },
+  boasVindas: {
+    marginTop: 24,
+    fontSize: 26,
+    lineHeight: 42,
+    fontWeight: 'bold',
+  },
+  legenda: {
+    fontSize: 16,
+    lineHeight: 26,
+  },
+});
+
+export default styles;
