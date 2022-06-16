5c410dcb9d6a42fd52ea447f8a42bf27e7aac8a9
AshileySabah <ashileysabrina2002@gmail.com>
2022-06-15 21:40:23 -0300

    feat: implementacao useReducer

--- src/pages/Home/Produtor/index.js
+++ src/pages/Home/Produtor/index.js
@@ -1,15 +1,13 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import {Text, View, Image, TouchableOpacity} from 'react-native';
 import Estrelas from '../../../components/Estrelas';
 import styles from './styles';
 
 const Produtor = ({nome, imagem, distancia, estrelas}) => {
-  const [selecionado, setSelecionado] = useState(false);
+  const [selecionado, toogleSelecionado] = useReducer(value => !value, false);
 
   return (
-    <TouchableOpacity
-      style={styles.cartao}
-      onPress={() => setSelecionado(!selecionado)}>
+    <TouchableOpacity style={styles.cartao} onPress={toogleSelecionado}>
       <Image style={styles.imagem} source={imagem} accessibilityLabel={nome} />
       <View style={styles.informacoes}>
         <View>
