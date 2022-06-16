56aebe856bf0ea3644ba28d316de0a80a7f97411
AshileySabah <ashileysabrina2002@gmail.com>
2022-06-16 20:10:02 -0300

    feat: produtores com filtro

--- src/pages/Home/Produtores/index.js
+++ src/pages/Home/Produtores/index.js
@@ -1,39 +1,19 @@
 import React, {useState} from 'react';
-import {FlatList, Text, TouchableOpacity} from 'react-native';
+import {FlatList, Text} from 'react-native';
 import produtores from '../../../mocks/produtores';
 import Produtor from '../Produtor';
+import Filtro from '../Filtro';
 import Topo from '../Topo';
 import styles from './styles';
 
 const Produtores = () => {
   const [lista, setLista] = useState(produtores.lista);
 
-  const ordenar = propriedade => {
-    const copyLista = lista.slice(0, lista.length);
-    const newLista = copyLista.sort((a, b) => {
-      return a[propriedade] < b[propriedade] ? -1 : 1;
-    });
-
-    if (JSON.stringify(lista) === JSON.stringify(newLista)) {
-      setLista(newLista.reverse());
-    } else {
-      setLista(newLista);
-    }
-  };
-
   const TopoLista = () => (
     <>
       <Topo />
       <Text style={styles.titulo}>Produtores</Text>
-      <TouchableOpacity onPress={() => ordenar('nome')}>
-        <Text>Ordenar por nome</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => ordenar('distancia')}>
-        <Text>Ordenar por distância</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => ordenar('estrelas')}>
-        <Text>Ordenar por estrelas</Text>
-      </TouchableOpacity>
+      <Filtro lista={lista} setLista={setLista} />
     </>
   );
 
