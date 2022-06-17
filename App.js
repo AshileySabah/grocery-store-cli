ee749dcc9a615ba45c054ce8deaee7ab9291e080
AshileySabah <ashileysabrina2002@gmail.com>
2022-06-17 19:44:42 -0300

    feat: home navigation

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {StyleSheet, SafeAreaView, StatusBar} from 'react-native';
+
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
 import Home from './src/pages/Home';
+import MelhoresProdutores from './src/pages/MelhoresProdutores';
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <SafeAreaView style={styles.tela}>
       <StatusBar />
       <NavigationContainer>
-        <Home />
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen
+            name="Melhores produtores"
+            component={MelhoresProdutores}
+          />
+        </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaView>
   );
