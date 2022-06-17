65ccd3d29eca8b8c5acf264c81607cf1d208caa5
AshileySabah <ashileysabrina2002@gmail.com>
2022-06-17 19:29:20 -0300

    feat: melhores produtores

---
+++ src/pages/MelhoresProdutores/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ListaProdutores from '../../components/ListaProdutores';
+
+const MelhoresProdutores = () => {
+  return <ListaProdutores melhoresProdutores />;
+};
+
+export default MelhoresProdutores;
