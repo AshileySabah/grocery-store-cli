cede0fd373b35c449e42491fbac1c6ae79d2fa72
AshileySabah <ashileysabrina2002@gmail.com>
2022-06-21 22:09:50 -0300

    feat: icones no bottom navigation

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -5,12 +5,28 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import ProdutorRoutes from '../routes/ProdutorRoutes';
 import MelhoresProdutoresRoutes from './MelhoresProdutoresRoutes';
 
+import Coracao from '../assets/coracao.svg';
+import Home from '../assets/home.svg';
+
 const Tab = createBottomTabNavigator();
 
 const AppRoutes = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{headerShown: false}}>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          headerShown: false,
+          tabBarActiveTintColor: '#2a9f85',
+          tabBarInactiveTintColor: '#c7c7c7',
+          tabBarIcon: ({color}) => {
+            let Icon = Home;
+            if (route.name === 'Melhores Produtores') {
+              Icon = Coracao;
+            }
+
+            return <Icon color={color} />;
+          },
+        })}>
         <Tab.Screen name="Home" component={ProdutorRoutes} />
         <Tab.Screen
           name="Melhores Produtores"
