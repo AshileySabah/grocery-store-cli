612c7d7c03c6dbe827257611f8de6b288c5c72ce
AshileySabah <ashileysabrina2002@gmail.com>
2022-06-21 21:25:31 -0300

    refact: top com svg

--- src/pages/Produtor/Topo/index.js
+++ src/pages/Produtor/Topo/index.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
-import {Image, TouchableOpacity} from 'react-native';
+import {Image, TouchableOpacity, Dimensions} from 'react-native';
 
 import Texto from '../Texto';
 
-import gradiente from '../../../assets/gradiente.svg';
+import Gradiente from '../../../assets/gradiente.svg';
 import topo from '../../../assets/topo.png';
-import voltarSVG from '../../../assets/voltar.svg';
+import VoltarSVG from '../../../assets/voltar.svg';
 
 import {styles as stylesFunction} from './styles';
 
+const largura = Dimensions.get('screen').width;
 const ALTURA_PADRAO = 270;
 
 export default function Topo({titulo, imagem = topo, altura = ALTURA_PADRAO}) {
   const navigation = useNavigation();
   const styles = stylesFunction(altura);
-
   return (
     <>
       <Image source={imagem} style={styles.topo} />
-      <Image source={gradiente} style={styles.gradiente} />
-
+      <Gradiente
+        width={largura}
+        height={(130 / 360) * largura}
+        style={styles.gradiente}
+      />
       <Texto style={styles.titulo}>{titulo}</Texto>
       <TouchableOpacity
         onPress={() => {
           navigation.goBack();
         }}
         style={styles.botaoVoltar}>
-        <Image source={voltarSVG} style={styles.voltar} />
+        <VoltarSVG color="white" style={styles.voltar} />
       </TouchableOpacity>
     </>
   );
