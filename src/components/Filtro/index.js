8dd5341b5f92f70d9f82bde4961d578ce2da4a83
AshileySabah <ashileysabrina2002@gmail.com>
2022-06-24 22:27:01 -0300

    refact: filtro

--- src/components/Filtro/index.js
+++ src/components/Filtro/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View} from 'react-native';
-import Button from '../../Button';
+import Button from './Button';
 import styles from './styles';
 
 const Filtro = ({lista, setLista}) => {
